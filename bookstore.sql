53edd636581c47b73dd2298fe5b79f6bf5dbd01c
SimamkeleSefalane <4270511@myuwc.ac.za>
2025-04-13 20:36:05 +0200

    Submission for Bookstore_bd Assignment

---
+++ bookstore.sql
@@ -0,0 +1,148 @@
+-- Drop and Create BookStoreDB -- 
+
+DROP DATABASE BookStoreDB;
+CREATE DATABASE BookStoreDB;
+USE BookStoreDB;
+
+-- Step 1: Insert new publisher and lannguage
+INSERT INTO publisher (pub_name) VALUES ('Skyline Publishers');      
+INSERT INTO book_language (language_name) VALUES ('Afrikaans');        
+
+-- Step 2: Insert the book
+INSERT INTO book (title, publisher_id, language_id, price, publication_year)
+VALUES ('Winds of Tomorrow', 1, 1, 320.00, 2015);             
+
+-- Step 3: Insert a new author
+INSERT INTO author (name) VALUES ('Lerato Mokoena');           
+
+-- Step 4: Link the new book and author
+INSERT INTO book_author (book_id, author_id)
+VALUES (1, 1);
+
+SELECT * FROM Book;
+
+-- List books and their authors
+SELECT b.title, a.name
+FROM book b
+JOIN book_author ba ON b.book_id = ba.book_id
+JOIN author a ON ba.author_id = a.author_id;
+
+
+-- 1. book_language table --
+CREATE TABLE book_language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(50)
+);
+
+-- 2. publisher table --
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    pub_name VARCHAR(255) NOT NULL
+);
+-- 3. Book Table --
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    publisher_id INT,
+    language_id INT,
+    price DECIMAL(10,2),
+    publication_year YEAR    
+    
+);
+-- 2. author table
+CREATE TABLE author (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- 3. book_author table (many-to-many)
+CREATE TABLE book_author (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES book(book_id),
+    FOREIGN KEY (author_id) REFERENCES author(author_id)
+);
+
+-- 6. country table --
+CREATE TABLE country (
+    country_id INT AUTO_INCREMENT PRIMARY KEY,
+    country_name VARCHAR(100)
+);
+
+-- 7. address table --
+CREATE TABLE address (
+    address_id INT AUTO_INCREMENT PRIMARY KEY,
+    street VARCHAR(255),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    zip_code VARCHAR(20),
+    country_id INT,
+    FOREIGN KEY (country_id) REFERENCES country(country_id)
+);
+
+-- 8. address_status table --
+CREATE TABLE address_status (
+    address_status_id INT AUTO_INCREMENT PRIMARY KEY,
+    status_description VARCHAR(50)
+);
+-- 9. customer table
+CREATE TABLE customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(255)
+);
+
+-- 10. customer_address table --
+CREATE TABLE customer_address (
+    customer_id INT,
+    address_id INT,
+    address_status_id INT,
+    PRIMARY KEY (customer_id, address_id),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (address_id) REFERENCES address(address_id),
+    FOREIGN KEY (address_status_id) REFERENCES address_status(address_status_id)
+);
+-- 11 shipping method --
+CREATE TABLE shipping_method (
+    shipping_method_id INT AUTO_INCREMENT PRIMARY KEY,
+    method_name VARCHAR(100)
+);
+
+-- 12. order_status table --
+CREATE TABLE order_status (
+    order_status_id INT AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(50)
+    
+);
+
+-- 13 customer_order --
+CREATE TABLE customer_order (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    shipping_method_id INT,
+    order_status_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (shipping_method_id) REFERENCES shipping_method(shipping_method_id)
+);
+-- 14. order_line table --
+CREATE TABLE order_line (
+    order_id INT,
+    book_id INT,
+    quantity INT,
+    PRIMARY KEY (order_id, book_id),
+    FOREIGN KEY (order_id) REFERENCES cust_order(order_id),
+    FOREIGN KEY (book_id) REFERENCES book(book_id)
+);
+
+-- 15. order_history table --
+CREATE TABLE order_history (
+    history_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    status_change_date DATE,
+    order_status_id INT,
+    FOREIGN KEY (order_id) REFERENCES cust_order(order_id),
+    FOREIGN KEY (order_status_id) REFERENCES order_status(order_status_id)
+);
